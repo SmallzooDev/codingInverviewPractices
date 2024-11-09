2ffeb9be8b51668403db95f278e3d140dea37985
smallzooDev <smallzooDev@gmail.com>
2024-11-09 18:46:38 +0900

    first commit

--- problems/problem_1-A/main.cpp
+++ problems/problem_1-A/main.cpp
@@ -1,21 +1,39 @@
 #include "../../common/common_headers.h"
+#include <numeric>
+// #include <bits/stdc++.h>
 using namespace std;
 
-vector<int> dwarfs;
-int height;
+pair<int, int> findFakeDwarfs(vector<int> &dwarfs) {
+  int sum = accumulate(dwarfs.begin(), dwarfs.end(), 0);
+  for (int i = 0; i < 9; i++) {
+    for (int j = 0; j < i; j++) {
+      if (sum - dwarfs[i] - dwarfs[j] == 100)
+        return pair(i, j);
+    }
+  }
 
+  return pair(-1, -1);
+}
 int main() {
+  vector<int> dwarfs;
+  int height;
+  vector<int> result;
+
   for (int i = 0; i < 9; i++) {
     cin >> height;
     dwarfs.push_back(height);
   }
 
-  sort(dwarfs.begin(), dwarfs.end());
-  auto start = dwarfs.begin();
+  pair<int, int> fake_dewarfs = findFakeDwarfs(dwarfs);
 
-  for (auto &dwarf : dwarfs) {
-    cout << dwarf;
+  for (int i = 0; i < 9; i++) {
+    if (i != fake_dewarfs.first && i != fake_dewarfs.second)
+      result.push_back(dwarfs[i]);
   }
 
+  sort(result.begin(), result.end());
+
+  for (auto i : result)
+    cout << i << ' ';
   return 0;
 }
